b1f61270cbe1658dfcf7e37fc931430e9a48baab
alimirjahani7@gmail.com <Alimirij7>
2018-12-08 19:36:46 +0330

    finding vars

--- parser.py
+++ parser.py
@@ -1,63 +1,101 @@
-import re
 import sys
 
-text = open(sys.argv[-1], 'r').read()
-
-variables = {}
-tokens = {}
-RHST = {}
-firsts = {}
-follows = {}
-predicts = {}
-parseTable = []
+text = open(sys.argv[-1], 'r')
 
 
-# parseStack = Stack()
-# semanticStack = Stack()
-
-
-# scanner = Parser(text, 'new_token', FINAL_STATES, TRANSITIONS, KEYWORDS)
-# scanner_result = scanner.run()
 class Parser:
+	# parseStack = Stack()
+	# semanticStack = Stack()
+	grammers = []
+	terminals_none_terminals = []
+	variables = []
+	tokens = []
+	RHST = []
+	firsts = {}
+	follows = {}
+	predicts = {}
+	parseTable = []
 
-	def parseCode(self):
+	@staticmethod
+	def is_variable(st):
+		if st== '->':
+			return False
+		if st == st.upper() and '@' != st[0]:
+			return True
+		return False
+
+	@staticmethod
+	def is_semantic_rule(st):
+		if st== '->':
+			return False
+		if '@' == st[0]:
+			return True
+		return False
+
+	@staticmethod
+	def is_terminal(st):
+		if st== '->':
+			return False
+		if '@' == st.lower() and '@' == st[0]:
+			return True
+		return False
+
+	def parse_code(self):
 		return
 
-	def fillParseTable(self):
+	def fill_parse_table(self):
 		return
 
-	def findAllPredicts(self):
+	def find_all_predicts(self):
 		return
 
 	def follow(self):
 		return
 
-	def findAllFollows(self):
+	def find_all_follows(self):
 		return
 
 	def first(self):
 		return
 
-	def findAllFirsts(self):
+	def find_all_firsts(self):
 		return
 
-	def findAllNullable(self):
+	def find_all_nullable(self):
 		return
 
-	def readGrammer(self):
+	def find_variables(self):
+		for t in self.terminals_none_terminals:
+			if self.is_variable(t):
+				self.variables.append(t)
 		return
 
-	def run(self):
-		# ret = []
-		# while self.current_char_idx < len(text):
-		# 	token = self._next_token()
-		# 	if token and len(token.value) > 0:
-		# 		ret.append(token)
-		# return ret
+	def read_grammers(self, text):
+		while True:
+			a = text.readline()
+			if "" == a:
+				break
+			else:
+				print(a.strip())
+				self.grammers.append(a.strip())
+		print(self.grammers)
+		for gra in self.grammers:
+			g = gra.split(" ")
+			for g1 in g:
+				self.terminals_none_terminals.append(g1)
+
+			print(g)
+		print(self.terminals_none_terminals)
+		self.find_variables()
+		print(self.variables)
+		return
+
+	def run(self, text):
+		self.read_grammers(text)
 		return
 
 
 parser = Parser()
-parser.run()
+parser.run(text)
 
-print(text)
+# print(text)
