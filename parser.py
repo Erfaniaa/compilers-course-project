914c317ee59973168419febed922f7dde9891f95
Erfan Alimohammadi <erfan.aa@gmail.com>
2018-12-17 07:44:25 +0330

    Refactor parser

--- parser.py
+++ parser.py
@@ -1,145 +1,124 @@
-import copy as copy
+from utils import add_element_to_set, add_list_of_elements_to_set
+import copy
 import re
 import sys
 
-text = open(sys.argv[-1], 'r')
-
-
-class Set:
-	def __init__(self):
-		self.data = []
-
-	def push(self, item):
-		if item not in self.data:
-			self.data.append(item)
-			return True
-		return False
-
-	def __getitem__(self, index):
-		return self.data[index]
-
-	def __contains__(self, item):
-		return item in self.data
-
-	def push_all(self, items):
-		update = False
-		for x in items:
-			update = update or self.push(x)
-		return update
-
-	def __len__(self):
-		return len(self.data)
-
-	def __str__(self):
-		return str(self.data)
-
 
 class Parser:
-	grammers = {}
-	rules = {}
-	grammers2 = {}
-	firsts = {}
-	variables = Set()
-	terminals = Set()
-	follows = {}
-	predicts = {}
-	parseTable = {}
-	nullables = Set()
+	_ARROW_STRING = "->"
+	_NIL_STRING = "nil"
+	_INVALID = -1
+	_SEMANTIC_RULE_CHARACTER = '@'
+
 
 	@staticmethod
-	def log_array_of_set(data):
-		print("+++++++++++")
-		for key in data:
-			print(key, end=' '),
-			print(" -> ", end=''),
-			output = ""
-			for andis in range(0, len(data[key])):
-				output += str(data[key][andis] + " ")
-			print(output)
-		print("----------")
-
-	@staticmethod
-	def is_variable(st):
-		pattern = r'[^\.a-zA-z]'
-		if re.search(pattern, st):
+	def is_variable(s):
+		if len(s) == 0:
 			return False
-		if st == '->':
+		if s == Parser._NIL_STRING:
 			return False
-		if st == st.upper() and '@' != st[0]:
+		pattern = r'[^\.a-zA-Z]'
+		if re.search(pattern, s):
+			return False
+		if s == Parser._ARROW_STRING:
+			return False
+		if s == s.upper() and s[0] != Parser._SEMANTIC_RULE_CHARACTER:
 			return True
 		return False
 
+
 	@staticmethod
-	def is_semantic_rule(st):
-		if st == '->':
+	def is_semantic_rule(s):
+		if len(s) == 0:
 			return False
-		if '@' == st[0]:
+		if s == Parser._ARROW_STRING:
+			return False
+		if s[0] == Parser._SEMANTIC_RULE_CHARACTER:
 			return True
 		return False
 
+
 	@staticmethod
-	def is_terminal(st):
-		if st == '->':
+	def is_terminal(s):
+		if len(s) == 0:
 			return False
-		if st == st.lower() and '@' != st[0]:
+		if s == Parser._ARROW_STRING:
+			return False
+		if s == s.lower() and s[0] != Parser._SEMANTIC_RULE_CHARACTER:
 			return True
 		return False
+	
 
-	def parse_code(self):
+	def __init__(self):
+		self.clear()
+
+
+	def clear(self):
+		self._variables = []
+		self._nullable_variables = []
+		self._terminals = []
+		self._variable_rules = {}
+		self._variable_rules_with_id = {}
+		self._rules = []
+		self._firsts = {}
+		self._follows = {}
+		self._predicts = {}
+		self._parse_table = {}
+
+
+	def _is_nullable_variable(self, variable):
+		return variable in self._nullable_variables
+
+
+	def _parse_code(self):
 		return
 
-	def fill_parse_table(self):
-		for var in self.variables:
-			self.parseTable[var] = {}
-			for ter in self.terminals:
-				self.parseTable[var][ter] = 0
-		for rule_id in self.rules:
-			var = self.rules[rule_id][0]
-			for ter in self.terminals:
-				if ter in self.predicts[rule_id]:
-					if self.parseTable[var][ter] != 0:
-						print("your grammer is not LL1")
-						print(str(var) + "--" + str(ter))
-						return False
-					else:
-						self.parseTable[var][ter] = rule_id
-		return True
 
-	def find_all_predicts(self):
-		for rule_id in self.rules:
-			pre = Set()
+	def _fill_parse_table(self):
+		for variable in self._variables:
+			self._parse_table[variable] = {}
+			for terminal in self._terminals:
+				self._parse_table[variable][terminal] = Parser._INVALID
+		for rule_id in range(len(self._rules)):
+			variable = self._rules[rule_id][0]
+			for terminal in self._terminals:
+				if terminal in self._predicts[rule_id]:
+					if self._parse_table[variable][terminal] != Parser._INVALID:
+						return (False, "The grammar is not LL1. Variable: " + str(variable) + " Terminal: " + str(terminal))
+					else:
+						self._parse_table[variable][terminal] = rule_id
+		return (True, "Parse table filled successfully.")
+
+
+	def _find_all_predicts(self):
+		for rule_id in range(len(self._rules)):
+			self._predicts[rule_id] = set()
 			idx = 0
-			is_right_null_able = True
-			for right in self.rules[rule_id]:
+			is_right_nullable = True
+			for right in self._rules[rule_id]:
 				idx += 1
 				if idx == 1:
 					continue
 				if self.is_terminal(right):
-					pre.push(right)
-					is_right_null_able = False
+					self._predicts[rule_id].add(right)
+					is_right_nullable = False
 					break
 				if self.is_variable(right):
-					pre.push_all(self.firsts[right])
-					if not self.is_null_able(right):
-						is_right_null_able = False
+					self._predicts[rule_id] |= set(self._firsts[right])
+					if not self._is_nullable_variable(right):
+						is_right_nullable = False
 						break
-			if is_right_null_able:
-				pre.push_all(self.follows[self.rules[rule_id][0]])
-			self.predicts[rule_id] = pre
-		return
+			if is_right_nullable:
+				self._predicts[rule_id] |= set(self._follows[self._rules[rule_id][0]])
 
-	def is_null_able(self, var):
-		return var in self.nullables
 
-	def find_all_follows(self):
-		for var in self.variables:
-			self.follows[var] = Set()
+	def _find_all_follows(self):
+		for variable in self._variables:
+			self._follows[variable] = set()
 		while True:
-			update = False
-			for var in self.variables:
-				fol = self.follows[var]
-				for rule_id in self.rules:
-					rule = self.rules[rule_id]
+			follows_updated = False
+			for variable in self._variables:
+				for rule in self._rules:
 					next_one_first_should_be_in_var_follows = False
 					idx = 0
 					for x in rule:
@@ -148,125 +127,173 @@ class Parser:
 							continue
 						if next_one_first_should_be_in_var_follows:
 							if self.is_variable(x):
-								update = update or fol.push_all(self.firsts[x])
-								if not self.is_null_able(x):
+								follows_updated |= add_list_of_elements_to_set(self._follows[variable], self._firsts[x])
+								if not self._is_nullable_variable(x):
 									next_one_first_should_be_in_var_follows = False
 							if self.is_terminal(x):
 								next_one_first_should_be_in_var_follows = False
-								update = update or fol.push(x)
+								follows_updated |= add_element_to_set(self._follows[variable], x)
 						if not next_one_first_should_be_in_var_follows:
-							if x == var:
+							if x == variable:
 								next_one_first_should_be_in_var_follows = True
 							continue
 					if next_one_first_should_be_in_var_follows:
-						update = update or fol.push_all(self.follows[rule[0]])
-			if not update:
+						follows_updated |= add_list_of_elements_to_set(self._follows[variable], self._follows[rule[0]])
+			if not follows_updated:
 				break
 
-	def find_all_firsts(self):
-		for var in self.variables:
-			self.firsts[var] = Set()
+
+	def _find_all_firsts(self):
+		for variable in self._variables:
+			self._firsts[variable] = set()
 		while True:
-			update = False
-			for var in self.variables:
-				for gra in self.grammers[var]:
-					for right in gra:
-						t = self.firsts[var]
-						if self.is_terminal(right) and right != "nill":
-							update = update or t.push(right)
+			firsts_updated = False
+			for variable in self._variables:
+				for rule in self._variable_rules[variable]:
+					for right in rule:
+						if self.is_terminal(right) and right != Parser._NIL_STRING:
+							firsts_updated |= add_element_to_set(self._firsts[variable], right)
 						if self.is_variable(right):
-							update = update or t.push_all(self.firsts[right])
-						self.firsts[var] = t
-						if self.is_variable(right) and self.is_null_able(right):
+							firsts_updated |= add_list_of_elements_to_set(self._firsts[variable], self._firsts[right])
+						if self.is_variable(right) and self._is_nullable_variable(right):
 							continue
 						break
-			if not update:
+			if not firsts_updated:
 				break
 
-	def find_all_nullable(self):
+
+	def _find_all_nullable_variables(self):
 		while True:
 			update = False
-			for var in self.variables:
-				if var in self.nullables:
+			for variable in self._variables:
+				if variable in self._nullable_variables:
 					continue
-				for gra in self.grammers[var]:
+				for rule in self._variable_rules[variable]:
 					all_nullables = True
-					for right in gra:
+					for right in rule:
 						if self.is_terminal(right):
 							all_nullables = False
-						elif self.is_variable(right) and right not in self.nullables:
+						elif self.is_variable(right) and right not in self._nullable_variables:
 							all_nullables = False
 						if not all_nullables:
 							break
 					if not all_nullables:
 						continue
-					elif var not in self.nullables:
-						self.nullables.push(var)
+					elif variable not in self._nullable_variables:
+						self._nullable_variables.append(variable)
 						update = True
 			if not update:
 				break
 		return
 
-	def find_variables(self, g):
-		for t in g:
-			if self.is_variable(t):
-				self.variables.push(t)
 
-	def find_terminals(self, g):
-		for t in g:
-			if self.is_terminal(t):
-				self.terminals.push(t)
+	def _update_variables(self, l):
+		for x in l:
+			if self.is_variable(x) and x not in self._variables:
+				self._variables.append(x)
 
-	def make_grammers(self, lines):
-		idx = 0
-		for gra in lines:
-			idx += 1
-			g = gra.split(" ")
-			self.find_variables(g)
-			self.find_terminals(g)
-			if len(g) < 3:
-				print("not enough word ing grammer " + str(idx))
-				return False
-			if not self.is_variable(g[0]) or g[1] != '->':
-				print("error in grammer " + str(idx))
-				return False
-			if len(g) == 3 and g[2] == "nill":
-				self.nullables.push(g[0])
-			key = g[0]
-			del g[1]
-			self.rules[idx] = copy.deepcopy(g)
-			del g[0]
-			temp = []
-			if key in self.grammers:
-				temp = self.grammers[key]
-			temp.append(g)
-			if key not in self.grammers2:
-				self.grammers2[key] = {}
-			self.grammers2[key][idx] = g
-			self.grammers[key] = temp
-		return True
 
-	def read_grammers(self, text):
+	def _update_terminals(self, l):
+		for x in l:
+			if self.is_terminal(x) and x not in self._terminals:
+				self._terminals.append(x)
+
+
+	def _update_grammar(self, rule_text):
+		idx = len(self._rules)
+		rule_text_tokens = rule_text.split(" ")
+		self._update_variables(rule_text_tokens)
+		self._update_terminals(rule_text_tokens)
+		if len(rule_text_tokens) < 3:
+			return (False, "Error in rule number " + str(idx))
+		if not self.is_variable(rule_text_tokens[0]) or rule_text_tokens[1] != Parser._ARROW_STRING:
+			return (False, "Error in rule number " + str(idx))
+		if len(rule_text_tokens) == 3 and rule_text_tokens[2] == Parser._NIL_STRING and rule_text_tokens[0] not in self._nullable_variables:
+			self._nullable_variables.append(rule_text_tokens[0])
+		key = rule_text_tokens[0]
+		del rule_text_tokens[1]
+		self._rules.append(copy.deepcopy(rule_text_tokens))
+		del rule_text_tokens[0]
+		temp_list = []
+		if key in self._variable_rules:
+			temp_list = self._variable_rules[key]
+		temp_list.append(rule_text_tokens)
+		if key not in self._variable_rules_with_id:
+			self._variable_rules_with_id[key] = {}
+		self._variable_rules_with_id[key][idx] = rule_text_tokens
+		self._variable_rules[key] = temp_list
+		return (True, "Grammar updated successfully.")
+
+
+	def _make_grammar_from_text(self, text):
 		lines = []
 		while True:
-			a = text.readline()
-			if "" == a:
+			line = text.readline().strip()
+			if not line or line == "":
 				break
 			else:
-				lines.append(a.strip())
-		return self.make_grammers(lines)
+				if not self._update_grammar(line):
+					return (False, "Error in making grammar")
+		return (True, "Grammar made successfully.")
+
 
 	def run(self, text):
-		if not self.read_grammers(text):
-			return
-		self.find_all_nullable()
-		self.find_all_firsts()
-		self.find_all_follows()
-		self.find_all_predicts()
-		if not self.fill_parse_table():
-			return
-		print("your grammer is LL1")
+		self.clear()
+		if not self._make_grammar_from_text(text)[0]:
+			return (False, "Error in making grammar")
+		self._find_all_nullable_variables()
+		self._find_all_firsts()
+		self._find_all_follows()
+		self._find_all_predicts()
+		if not self._fill_parse_table()[0]:
+			return (False, "Error in filling parse table")
+		return (True, "Parser ran successfully.")
+
+
+	def get_firsts(self):
+		return self._firsts
+
+
+	def get_follows(self):
+		return self._follows
+
+
+	def get_variables(self):
+		return self._variables
+
+
+	def get_terminals(self):
+		return self._terminals
+
+
+	def get_parse_table(self):
+		return self._parse_table
+
+
+	def get_rules(self):
+		return self._rules
+
+
+	def get_variable_rules(self):
+		return self._variable_rules
+
+
+	def get_variable_rules_with_id(self):
+		return self._variable_rules_with_id
+
+
+	def get_nullable_variables(self):
+		return self._nullable_variables
 
 
 parser = Parser()
+text = open(sys.argv[-1], 'r')
 parser.run(text)
+
+print(parser.get_firsts())
+print(parser.get_follows())
+print(parser.get_variables())
+print(parser.get_parse_table())
+print(parser.get_rules())
+print(parser.get_variable_rules())
+print(parser.get_variable_rules_with_id())
---
+++ utils.py
@@ -0,0 +1,12 @@
+def add_element_to_set(cur_set, element):
+	if (len(cur_set | set([element])) > len(cur_set)):
+		cur_set.add(element)
+		return True
+	return False
+
+def add_list_of_elements_to_set(cur_set, list_of_elements):
+	tmp_set = cur_set | set(list_of_elements)
+	if (len(tmp_set) > len(cur_set)):
+		cur_set |= tmp_set
+		return True
+	return False
