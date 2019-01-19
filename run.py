4ccec176c42fc0929e471c2603479c683ceaab48
Erfan Alimohammadi <erfan.aa@gmail.com>
2019-01-19 15:50:08 +0330

    Remove GUI

--- run.py
+++ run.py
@@ -6,37 +6,22 @@ from scanner import Scanner
 from scanner import FINAL_STATES, TRANSITIONS, KEYWORDS
 
 GRAMMAR_FILE_PATH = "grammar.in"
-
-def open_file():
-	source_code_file_path = askopenfilename(initialdir=".", filetypes =(("C++", "*.cpp"),("All Files","*.*")), title = "Choose a C++ source file.")
-	if not source_code_file_path or source_code_file_path == "":
-		return
-	text = open(source_code_file_path, 'r').read()
-	scanner = Scanner(text, 'new_token', FINAL_STATES, TRANSITIONS, KEYWORDS)
-	scanner_result = scanner.run()
-	print(scanner_result)
-	parser = Parser("PROGRAM")
-	print(parser.run(open(GRAMMAR_FILE_PATH, 'r')))
-	print(parser.get_variables())
-	# print(parser.get_nullable_variables())
-	# print(parser.get_rules())
-	# print(parser.get_firsts())
-	# print(parser.get_follows())
-	# print(parser.get_predicts())
-	# print(parser.get_parse_table())
-	parser_result = parser.match(scanner_result)
-	label.config(text=parser_result[1])
-
-
-root = Tk()
-root.geometry("400x300")
-Title = root.title("Compiler project")
-label = ttk.Label(root, text="Choose a c++ source file.", foreground="black", font=("Helvetica", 16))
-label.pack()
-menu = Menu(root)
-root.config(menu=menu)
-file = Menu(menu)
-file.add_command(label = 'Open', command = open_file)
-file.add_command(label = 'Exit', command = lambda: exit())
-menu.add_cascade(label = 'File', menu = file)
-root.mainloop()
+source_code_file_path = "test.cpp"
+if not source_code_file_path or source_code_file_path == "":
+	exit()
+text = open(source_code_file_path, 'r').read()
+scanner = Scanner(text, 'new_token', FINAL_STATES, TRANSITIONS, KEYWORDS)
+scanner_result = scanner.run()
+print(scanner_result)
+parser = Parser("PROGRAM")
+parser_run_result = parser.run(open(GRAMMAR_FILE_PATH, 'r'))
+if not parser_run_result[0]:
+	exit()
+print("variables:", parser.get_variables())
+# print(parser.get_nullable_variables())
+# print(parser.get_rules())
+# print(parser.get_firsts())
+# print(parser.get_follows())
+# print(parser.get_predicts())
+# print(parser.get_parse_table())
+parser_match_result = parser.match(scanner_result)
